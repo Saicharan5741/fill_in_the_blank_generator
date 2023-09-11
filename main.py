13cac0aa4e0ba9d00c1ef64d5338c0a825bc7f5a
Saicharan5741 <141002236+Saicharan5741@users.noreply.github.com>
2023-09-11 21:44:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,37 @@
-from flask import Flask, render_template, request, redirect, url_for
-import random
+import streamlit as st
 import re
 
-app = Flask(__name__)
-
-@app.route('/')
-def page1():
-    return render_template('index.html')
-
-@app.route('/page2', methods=['POST'])
-def page2():
-    text = request.form['text']
+# Page 1
+st.title("Fill in the Blank Generator")
+text = st.text_area("Enter text:")
+if st.button("Next"):
     words = text.split()
-    return render_template('page2.html', words=words, text=text)
+    st.session_state['words'] = words
+    st.session_state['text'] = text
+    st.session_state['step'] = 2
 
-@app.route('/page3', methods=['POST'])
-def page3():
-    selected_word = request.form['selected_word']
-    text = request.form['text']
-    result = re.sub(r'\b' + re.escape(selected_word) + r'\b', "___", text)
-    return render_template('page3.html', result=result)
+# Page 2
+if st.session_state.get('step') == 2:
+    st.subheader("Select a random word from the input:")
+    selected_word = st.selectbox("Choose a word:", st.session_state['words'])
+    if st.button("Submit"):
+        text = st.session_state['text']
+        result = re.sub(r'\b' + re.escape(selected_word) + r'\b', "___", text)
+        st.session_state['result'] = result
+        st.session_state['step'] = 3
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# Page 3
+if st.session_state.get('step') == 3:
+    st.subheader("The random clicked word is replaced with:")
+    st.write(st.session_state['result'])
+
+# You can add a "Go Back" button if needed
+if st.button("Go Back"):
+    st.session_state['step'] -= 1
+
+
+
+
 
 
 
